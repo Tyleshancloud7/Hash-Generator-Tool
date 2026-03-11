4ef7105b6fbd904fe224bd755497cc588b51c58f
Tylesha Alexanderr <tyleshashanee@icloud.com>
2026-03-11 09:48:19 -0400

    Add Hash Generator cybersecurity tool

---
+++ script.js
@@ -0,0 +1,16 @@
+async function generateHash() {
+
+let text = document.getElementById("inputText").value;
+
+const encoder = new TextEncoder();
+const data = encoder.encode(text);
+
+const hashBuffer = await crypto.subtle.digest("SHA-256", data);
+
+const hashArray = Array.from(new Uint8Array(hashBuffer));
+
+const hashHex = hashArray.map(b => b.toString(16).padStart(2, "0")).join("");
+
+document.getElementById("output").textContent = hashHex;
+
+}
